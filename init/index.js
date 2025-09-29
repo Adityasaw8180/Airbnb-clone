8b5b654990ffd572ebf5980c439ae8a055d077aa
Aditya Sawant <sawantaditya086@gmail.com>
2025-09-29 20:31:37 +0530

    new file added

---
+++ init/index.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const initData = require('./data')
+const Listing = require("../models/listing");
+
+const MONGO_DB = "mongodb://127.0.0.1:27017/airbnb"
+
+main().then(() => {
+    console.log('connected mongobd successfully');
+}).catch((err) => {
+    console.log(`connection failed ${err}`);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_DB);
+}
+
+const initDB = async ()=>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log('Data Initialized successfully')
+}
+
+initDB();
