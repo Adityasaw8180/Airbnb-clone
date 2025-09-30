99b545f0a5ae81995a8fa889d317aaf89323c515
Aditya Sawant <sawantaditya086@gmail.com>
2025-09-30 23:18:09 +0530

    navbar started

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.engine('ejs', ejsMate);
 
 // Middleware (only once)
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));  // Serve static files from the 'public' directory
 // View engine
 app.set('view engine', "ejs");
 app.set('views', path.join(__dirname, "views"));
