b67cabe932a3957b81cf5bcb32c6ecf09540184d
Aditya Sawant <sawantaditya086@gmail.com>
2025-10-02 23:41:21 +0530

    url default value set

--- models/listing.js
+++ models/listing.js
@@ -16,7 +16,7 @@ const listingSchema = new Schema({
         },
         url: {
             type: String,
-            set: (v) => v === "" ? "defaul link" : v,
+            set: (v) => v === "" ? "https://images.unsplash.com/photo-1614649024145-7f847b1c803f?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
         },
         
     },
